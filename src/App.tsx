0989ad38fb4baf298425c3eb26e4bdf6adafd7fa
Claudinei Pedrozo <cs.pedrozo33@gmail.com>
2024-09-10 00:17:14 -0400

    Painel RGBA funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import ColorPanel from './components/ColorPanel';
 
-function App() {
+
+const App: React.FC = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const [alpha, setAlpha] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <ColorPanel red={red} green={green} blue={blue} alpha={alpha} />
+      </div>
+      <div className={styles.input}>
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={255}
+          value={red}
+          onChange={(e) => setRed(Number(e.target.value))}
+        />        
+      </div>
+      
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={255}
+          value={green}
+          onChange={(e) => setGreen(Number(e.target.value))}
+        />        
+      </div>
+      
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={255}
+          value={blue}
+          onChange={(e) => setBlue(Number(e.target.value))}
+        />        
+      </div>
+      
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={255}
+          value={alpha}
+          onChange={(e) => setAlpha(Number(e.target.value))}
+        />        
+      </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ColorPanel.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface ColorPanelProps {
+  red: number;
+  green: number;
+  blue: number;
+  alpha: number;
+}
+
+const ColorPanel: React.FC<ColorPanelProps> = ({ red, green, blue, alpha }) => {
+  const colorStyle = {
+    backgroundColor: `rgba(${red}, ${green}, ${blue}, ${alpha})`,
+    width: '500px',
+    height: '500px',
+  };
+
+  return <div style={colorStyle} />;
+};
+
+export default ColorPanel;
